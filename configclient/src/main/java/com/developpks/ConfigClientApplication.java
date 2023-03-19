671e0de51a7de256dba55459e6916ada7331cc03
Pankaj K Singh <pankajpankaj2006@gmail.com>
2023-03-19 10:40:56 +0530

    Config-Client
    Spring boot config client demo

---
+++ configclient/src/main/java/com/developpks/ConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.developpks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@RefreshScope
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
